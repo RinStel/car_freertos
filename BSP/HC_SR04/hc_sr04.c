452c448c401fbf510cb7b4650cb71c21fee67314
RinStel <yschoin@gmail.com>
2026-04-21 15:19:43 +0000

    fix(HC-SR04): 修正超声波模块
    - 修正最大距离和触发延时
    - 优化距离计算逻辑

--- BSP/HC_SR04/hc_sr04.c
+++ BSP/HC_SR04/hc_sr04.c
@@ -8,8 +8,7 @@
 
 /*-----------------------------------------------------------*/
 
-#define HCSR04_MAX_DISTANCE_MM (4000U)
-#define ALPHA_FILTER 0.15f
+#define HCSR04_MAX_DISTANCE_MM 3000U
 
 /*-----------------------------------------------------------*/
 
@@ -23,26 +22,17 @@ volatile uint16_t           usHCSR04Distance_mm;
 
 /*-----------------------------------------------------------*/
 
-static inline float_t prvLowPassFilter(float_t old_value, float_t new_value, float_t alpha)
-{
-    return (alpha) * (new_value) + (1 - (alpha)) * (old_value);
-}
-
 void vHCSR04Trigger(void)
 {
-    taskENTER_CRITICAL();
     ucHCSR04EchoActive = 0U;
     // 发送至少10us的高电平触发信号
     DL_GPIO_setPins(GPIO_HCSR04_PORT, GPIO_HCSR04_TRIG_PIN);
-    delay_cycles(CPUCLK_FREQ / 1000000 * 10); // 10us 延时
+    vTaskDelay(1); // 1ms > 10us
     DL_GPIO_clearPins(GPIO_HCSR04_PORT, GPIO_HCSR04_TRIG_PIN);
-    taskEXIT_CRITICAL();
 }
 
 void vHCSR04Echo_IRQHandler(void)
 {
-    static volatile uint16_t usHCSR04LastDistance_mm = 0U;
-
     if (DL_GPIO_readPins(GPIO_HCSR04_PORT, GPIO_HCSR04_ECHO_PIN))
     {
         // 上升沿，记录开始时间
@@ -52,20 +42,23 @@ void vHCSR04Echo_IRQHandler(void)
     else if (ucHCSR04EchoActive != 0U)
     {
         // 下降沿，计算距离
-        uint16_t echoDuration_us = usFreeTimerGetElapsed(xHCSR04EchoStartTime);
+        uint32_t echoDuration_us = (uint32_t) usFreeTimerGetElapsed(xHCSR04EchoStartTime);
         ucHCSR04EchoActive       = 0U;
 
-        if (echoDuration_us <= (HCSR04_MAX_DISTANCE_MM * 2000U / 340U))
+        if (echoDuration_us <= (HCSR04_MAX_DISTANCE_MM * 2000 / 340))
         {
-            usHCSR04Distance_mm = (uint16_t) prvLowPassFilter(
-                usHCSR04LastDistance_mm, ((float_t) echoDuration_us * 340 / 2000), ALPHA_FILTER);
+            usHCSR04Distance_mm = (uint16_t) (echoDuration_us * 340 / 2000);
+        }
+        else
+        {
+            usHCSR04Distance_mm = HCSR04_MAX_DISTANCE_MM;
         }
     }
 }
 
 static void prvHCSR04Task(void *argument)
 {
-    const TickType_t xFrequency = pdMS_TO_TICKS(1000 / 30); // 30Hz
+    const TickType_t xFrequency = pdMS_TO_TICKS(1000 / 10); // 10Hz
     TickType_t       xLastWakeTime;
 
     xLastWakeTime = xTaskGetTickCount();
